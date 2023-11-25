45799bf1bff7e5f04d762a4fc4424becfba74275
Omid Shahbazi <sh.omid.m@gmail.com>
2023-11-25 17:33:50 +0330

    Modified   include/Debug.h

--- include/Debug.h
+++ include/Debug.h
@@ -4,41 +4,6 @@
 
 #include "Log.h"
 #include <esp_system.h>
-// #include "Plotter/Plotter.h"
-
-class Debug
-{
-public:
-	static void Initialize(void)
-	{
-#if __PLATFORMIO_BUILD_DEBUG__
-		// GetPlotter().Begin();
-#endif
-	}
-
-	template <typename A>
-	static void AddGraph(const char *Title, int PointsDisplayed, const char *Label, A &Value)
-	{
-#if __PLATFORMIO_BUILD_DEBUG__
-		// GetPlotter().AddTimeGraph(Title, PointsDisplayed, Label, Value);
-#endif
-	}
-
-	static void Plot(void)
-	{
-#if __PLATFORMIO_BUILD_DEBUG__
-		// GetPlotter().Plot();
-#endif
-	}
-
-private:
-	// static Plotter &GetPlotter(void)
-	// {
-	// 	static Plotter plotter;
-
-	// 	return plotter;
-	// }
-};
 
 #if __PLATFORMIO_BUILD_DEBUG__
 #define ASSERT(Expression, Tag, Message, ...)            \
