9722d66d5b5b996118161a6363e1a627422ae1bc
Omid Shahbazi <sh.omid.m@gmail.com>
2024-02-18 18:20:45 +0330

    Modified   include/DSP/Distortion.h Added      include/DSP/Phaser.h

--- include/DSP/Distortion.h
+++ include/DSP/Distortion.h
@@ -5,8 +5,7 @@
 #include "IDSP.h"
 #include "../Math.h"
 #include "../Debug.h"
-#include "../Filters/LowPassFilter.h"
-#include "../Filters/NoiseGateFilter.h"
+#include "../Filters/WaveShaperFilter.h"
 
 class Distortion : public IDSP
 {
@@ -17,6 +16,9 @@ public:
 		  m_Factor(0),
 		  m_Multiplier(0)
 	{
+		static WaveShaperFilter::TablePoints points[] = {{-1, -0.8}, {-0.5, -0.8}, {0, 0}, {0.5, 0.8}, {1, 0.8}};
+		m_WaveShaperFilter.SetTable(points, 5);
+
 		SetGain(1);
 		SetRate(1);
 	}
@@ -28,11 +30,7 @@ public:
 
 		m_Gain = Value;
 
-		m_Factor = Math::Lerp(0.001, 0.9, m_Gain);
-
-		printf("Gain %f\n", m_Factor);
-
-		// 0.001000
+		m_Factor = Math::Lerp(1.0, 4, m_Gain);
 	}
 	float GetGain(void)
 	{
@@ -46,11 +44,7 @@ public:
 
 		m_Rate = Value;
 
-		m_Multiplier = Math::Lerp(100.0, 1500, m_Rate);
-
-		printf("Rate %f\n", m_Multiplier);
-
-		// 693.623718
+		m_Multiplier = Math::Lerp(50.0, 400, m_Rate);
 	}
 	float GetRate(void)
 	{
@@ -60,10 +54,12 @@ public:
 	void ProcessBuffer(double *Buffer, uint16 Count) override
 	{
 		for (uint16 i = 0; i < Count; ++i)
-			Buffer[i] = Math::ExponentialSaturation(Buffer[i] * m_Multiplier, m_Factor) / m_Multiplier;
+			Buffer[i] = m_Factor * m_WaveShaperFilter.Process(Buffer[i] * m_Multiplier) / m_Multiplier;
 	}
 
 private:
+	WaveShaperFilter m_WaveShaperFilter;
+
 	float m_Gain;
 	float m_Rate;
 
---
+++ include/DSP/Phaser.h
@@ -0,0 +1,63 @@
+#pragma once
+#ifndef PHASER_H
+#define PHASER_H
+
+#include "IDSP.h"
+#include "../Math.h"
+#include "../Debug.h"
+#include "../Filters/OscillatorFilter.h"
+
+class Phaser : public IDSP
+{
+
+public:
+	Phaser(uint32 SampleRate)
+		: m_Oscillator(SampleRate),
+		  m_Depth(0),
+		  m_Rate(0)
+	{
+		SetDepth(0.5);
+		SetRate(1);
+	}
+
+	//[0, 1]
+	void SetDepth(float Value)
+	{
+		ASSERT(0 <= Value && Value <= 1, "Invalid Value");
+
+		m_Depth = Value;
+	}
+	float GetDepth(void)
+	{
+		return m_Depth;
+	}
+
+	//(0, 25]
+	void SetRate(float Value)
+	{
+		ASSERT(0 < Value && Value <= 25, "Invalid Value");
+
+		m_Oscillator.SetFrequency(Value);
+	}
+	float GetRate(void)
+	{
+		return m_Oscillator.GetFrequency();
+	}
+
+	void ProcessBuffer(double *Buffer, uint16 Count) override
+	{
+		for (uint16 i = 0; i < Count; ++i)
+		{
+			float modulation = (1 - m_Depth) + (m_Depth * 0.5 * (1 + m_Oscillator.Process()));
+
+			Buffer[i] *= modulation;
+		}
+	}
+
+private:
+	OscillatorFilter m_Oscillator;
+	float m_Depth;
+	float m_Rate;
+};
+
+#endif
